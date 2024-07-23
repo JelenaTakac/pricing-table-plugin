daa4c4da582a6bc013272a70edb55b467c4a4f7b
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-07-23 13:04:12 +0200

    Created EditComponent

--- bsf-pricing-table-plugin.php
+++ bsf-pricing-table-plugin.php
@@ -12,15 +12,23 @@ if (!defined('ABSPATH')) exit;
 
 class BsfPricingTablePlugin {
     function __construct() {
-        add_action('init', [$this, 'adminAssets']);
+        add_action('init', array($this, 'adminAssets'));
+        add_action('enqueue_block_editor_assets', array($this, 'myplugin_enqueue_dashicons'));
+
     }
 
     function adminAssets() {
+        wp_register_style('pricingblockstyle', plugin_dir_url(__FILE__) . 'build/index.css');
         wp_register_script('pricingblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element', 'wp-editor'));
-        register_block_type('myplugin/pricing-table', array(
+        register_block_type('my-plugin/pricing-table', array(
             'editor_script' => 'pricingblocktype',
-            'render_callback' => array($this, 'theHTML')
+            'editor_style' => 'pricingblockstyle',
+            'render_callback' => array($this, 'renderPricingTableBlock')
         ));
+    }
+
+    function myplugin_enqueue_dashicons() {
+        wp_enqueue_style('dashicons');
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-wp.blocks.registerBlockType('myplugin/pricing-table', {
+import "./index.scss";
+import { InspectorControls, RichText } from '@wordpress/block-editor';
+import { Button, PanelBody, SelectControl, TextControl, ToggleControl } from "@wordpress/components";
+
+wp.blocks.registerBlockType('my-plugin/pricing-table', {
     title: 'Pricing Table',
     icon: 'table-row-after',
     category: 'common',
-    edit: function () {
-        return (
-        <div>
-            <p>Hello, this is a paragraph.</p>
-            <h4>Hi there.</h4>
-        </div>
-        )
+    attributes: {
+        icon: {type: "string", default: "smiley"},
+        title: {type: "string", default: "Package Name"},
+        description: {type: "string", default: "Auctor condimentum vero, solutauld hilvil similique, nisl proin augue? Accumsan interdum etiam"},
+        
+        currentPrice: {
+            type: 'string',
+            default: '39.99',
+        },
+        currency: {
+            type: 'string',
+            default: '$',
+        },
+        hasDiscount: {
+            type: 'boolean',
+            default: false,
+        },
+        discountPrice: {
+            type: 'string',
+            default: '',
+        },
+
+
+        button: {type: "string" , default: "BUY NOW"}
     },
+    description: "Create your best pricing table.",
+    edit: EditComponent,
     save: function () {
         return (
         <>
@@ -19,3 +42,89 @@ wp.blocks.registerBlockType('myplugin/pricing-table', {
         )
     }
 });
+
+function EditComponent(props) {
+
+    function handleTitleChange(value) {
+        props.setAttributes({ title: value })
+    }
+
+    function handleDescriptionChange(value) {
+        props.setAttributes({ description: value })
+    }
+
+    function handleButtonChange(value) {
+        props.setAttributes({ button: value })
+    }
+
+    return (
+        <div className="pricing-table-edit-block">
+            <InspectorControls>
+                <PanelBody title="Icon Settings">
+                    <SelectControl
+                        label='Icon'
+                        value={props.attributes.icon}
+                        options={[
+                            { label: 'Smiley', value: 'smiley' },
+                            { label: 'Heart', value: 'heart' },
+                            { label: 'Lightbulb', value: 'lightbulb' },
+                        ]}
+                        onChange={(newIcon) => props.setAttributes({ icon: newIcon })}
+                    />
+                </PanelBody>
+
+
+                <PanelBody title="Price Settings">
+                    <TextControl
+                        label="Current Price"
+                        value={props.attributes.currentPrice}
+                        onChange={(newPrice) => props.setAttributes({ currentPrice: newPrice })}
+                    />
+                    <TextControl
+                        label="Currency"
+                        value={props.attributes.currency}
+                        onChange={(newCurrency) => props.setAttributes({ currency: newCurrency })}
+                    />
+                    <ToggleControl
+                        label="Has Discount"
+                        checked={props.attributes.hasDiscount}
+                        onChange={(newHasDiscount) => props.setAttributes({ hasDiscount: newHasDiscount })}
+                    />
+                    {props.attributes.hasDiscount && (
+                        <TextControl
+                            label="Discount Price"
+                            value={props.attributes.discountPrice}
+                            onChange={(newDiscountPrice) => props.setAttributes({ discountPrice: newDiscountPrice })}
+                        />
+                    )}
+                </PanelBody>
+
+            </InspectorControls>
+
+            <div className="icon-container">
+                <span className={`dashicons dashicons-${props.attributes.icon}`} />
+            </div>
+            {/* <h2>Hello from Edit block</h2> */}
+            <RichText allowedFormats={["core/bold", "core/italic"]} tagName="h3" className={`headline`} onChange={handleTitleChange} value={props.attributes.title}/>
+
+            <RichText allowedFormats={[]} tagName="p" className={`description`} onChange={handleDescriptionChange} value={props.attributes.description}/>
+
+
+            <div className="pricing-table-price">
+                {props.attributes.hasDiscount && props.attributes.discountPrice ? (
+                    <>
+                        <span className="previous-price">{props.attributes.currency}{props.attributes.currentPrice}</span>
+                        <span className="current-price">{props.attributes.currency}{props.attributes.discountPrice}</span>
+                    </>
+                ) : (
+                    <span className="current-price">{props.attributes.currency}{props.attributes.currentPrice}</span>
+                )}
+            </div>
+
+            <Button variant="primary">
+                <RichText allowedFormats={["core/bold", "core/italic"]} className={`btn`} value={props.attributes.button} onChange={handleButtonChange}/>
+            </Button>
+            
+        </div>
+    )
+}
