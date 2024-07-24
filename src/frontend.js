9c37e2cc9ec3b1d85a6a98ede2c5c629a8afd0d2
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-07-24 16:58:45 +0200

    Created an array of cards and the possibility of adding and deleting cards

--- src/frontend.js
+++ src/frontend.js
@@ -8,25 +8,32 @@ divPricingTable.forEach(block => {
     const data = JSON.parse(block.querySelector("pre").innerHTML);
     ReactDOM.render(<PricingTableFrontend {...data} />, block);
     block.classList.remove("pricing-tables");
-})
+});
 
 function PricingTableFrontend(props) {
     return (
-        <div className="pricing-table-frontend" style={{backgroundColor: props.bgColor, textAlign: props.theAlignment}}>
-            <span className={`dashicons dashicons-${props.icon}`}></span>
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
-            <div className="pricing-table-price">
-                {props.hasDiscount && props.discountPrice ? (
-                    <>
-                        <span className="previous-price">{props.currency}{props.currentPrice}</span>
-                        <span className="current-price">{props.currency}{props.discountPrice}</span>
-                    </>
-                ) : (
-                    <span className="current-price">{props.currency}{props.currentPrice}</span>
-                )}
+        <div className="pricing-table-frontend" style={{ backgroundColor: props.bgColor, textAlign: props.theAlignment }}>
+            <h2>{props.blockTitle}</h2>
+            <div className="cards-container">
+                {props.cards.map((card, index) => (
+                    <div key={index} className="card">
+                        <span className={`dashicons dashicons-${card.icon}`}></span>
+                        <h3>{card.title}</h3>
+                        <p>{card.description}</p>
+                        <div className="pricing-table-price">
+                            {card.hasDiscount && card.discountPrice ? (
+                                <>
+                                    <span className="previous-price">{card.currency}{card.currentPrice}</span>
+                                    <span className="current-price">{card.currency}{card.discountPrice}</span>
+                                </>
+                            ) : (
+                                <span className="current-price">{card.currency}{card.currentPrice}</span>
+                            )}
+                        </div>
+                        <button className="btn">{card.button}</button>
+                    </div>
+                ))}
             </div>
-            <button className="btn">{props.button}</button>
         </div>
-    )
+    );
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
 import "./index.scss";
 import { InspectorControls, RichText, BlockControls, AlignmentToolbar } from "@wordpress/block-editor";
 import { Button, PanelBody, PanelRow, SelectControl, TextControl, ToggleControl } from "@wordpress/components";
-import {ChromePicker} from "react-color";
+import { ChromePicker } from "react-color";
 
 wp.blocks.registerBlockType('my-plugin/pricing-table', {
     title: 'Pricing Table',
     icon: 'table-row-after',
     category: 'common',
     attributes: {
-        icon: {type: "string", default: "smiley"},
-        title: {type: "string", default: ""},
-        description: {type: "string", default: ""},
-        currentPrice: {type: "string", default: "39.99"},
-        currency: {type: "string",default: "$"},
-        hasDiscount: {type: "boolean", default: false},
-        discountPrice: {type: "string", default: ""},
-        button: {type: "string" , default: ""},
-        bgColor: {type: "string", default: "#ebebeb"},
-        theAlignment: {type: "string", default: "center"}
+        blockTitle: { type: "string", default: "Get Your Perfect Edition Plans" },
+        cards: {
+            type: "array",
+            default: [
+                {
+                    icon: "smiley",
+                    title: "",
+                    description: "",
+                    currentPrice: "39.99",
+                    currency: "",
+                    hasDiscount: false,
+                    discountPrice: "",
+                    button: "",
+                },
+            ],
+        },
+        bgColor: { type: "string", default: "#ebebeb" },
+        theAlignment: { type: "string", default: "center" },
     },
     description: "Create your best pricing table.",
     edit: EditComponent,
@@ -27,96 +35,146 @@ wp.blocks.registerBlockType('my-plugin/pricing-table', {
 });
 
 function EditComponent(props) {
+    const { attributes, setAttributes } = props;
 
-    function handleTitleChange(newValue) {
-        props.setAttributes({ title: newValue })
-    }
+    const handleCardChange = (newCard, index) => {
+        const updatedCards = [...attributes.cards];
+        updatedCards[index] = newCard;
+        setAttributes({ cards: updatedCards });
+    };
 
-    function handleDescriptionChange(newValue) {
-        props.setAttributes({ description: newValue })
-    }
+    const addNewCard = () => {
+        const newCard = {
+            icon: "smiley",
+            title: "",
+            description: "",
+            currentPrice: "39.99",
+            currency: "",
+            hasDiscount: false,
+            discountPrice: "",
+            button: "",
+        };
+        setAttributes({ cards: [...attributes.cards, newCard] });
+    };
 
-    function handleButtonChange(newValue) {
-        props.setAttributes({ button: newValue })
-    }
+    const removeCard = (index) => {
+        const updatedCards = attributes.cards.filter((card, cardIndex) => cardIndex !== index);
+        setAttributes({ cards: updatedCards });
+    };
 
     return (
-        <div className="pricing-table-edit-block" style={{backgroundColor: props.attributes.bgColor}}>
+        <div className="pricing-table-edit-block" style={{ backgroundColor: attributes.bgColor }}>
             <BlockControls>
-                <AlignmentToolbar value={props.attributes.theAlignment} onChange={x => props.setAttributes({theAlignment: x})} />
+                <AlignmentToolbar value={attributes.theAlignment} onChange={(newAlignment) => setAttributes({ theAlignment: newAlignment })} />
             </BlockControls>
             <InspectorControls>
-                <PanelBody title="Icon Settings">
-                    <SelectControl
-                        label="Icon"
-                        value={props.attributes.icon}
-                        options={[
-                            { label: "Smiley", value: "smiley" },
-                            { label: "Heart", value: "heart" },
-                            { label: "Lightbulb", value: "lightbulb" },
-                        ]}
-                        onChange={(newIcon) => props.setAttributes({ icon: newIcon })}
-                    />
-                </PanelBody>
-
-
-                <PanelBody title="Price Settings">
-                    <TextControl
-                        label="Current Price"
-                        value={props.attributes.currentPrice}
-                        onChange={(newPrice) => props.setAttributes({ currentPrice: newPrice })}
-                    />
-                    <TextControl
-                        label="Currency"
-                        value={props.attributes.currency}
-                        onChange={(newCurrency) => props.setAttributes({ currency: newCurrency })}
-                    />
-                    <ToggleControl
-                        label="Has Discount"
-                        checked={props.attributes.hasDiscount}
-                        onChange={(newHasDiscount) => props.setAttributes({ hasDiscount: newHasDiscount })}
-                    />
-                    {props.attributes.hasDiscount && (
-                        <TextControl
-                            label="Discount Price"
-                            value={props.attributes.discountPrice}
-                            onChange={(newDiscountPrice) => props.setAttributes({ discountPrice: newDiscountPrice })}
-                        />
-                    )}
-                </PanelBody>
-
-                <PanelBody title="Background Color" initialOpen={true}>
+                <PanelBody title="Background Color" initialOpen={false}>
                     <PanelRow>
-                        <ChromePicker color={props.attributes.bgColor} onChangeComplete={x => props.setAttributes({bgColor: x.hex})} disableAlpha={true}/>
+                        <ChromePicker
+                            color={attributes.bgColor}
+                            onChangeComplete={(color) => setAttributes({ bgColor: color.hex })}
+                            disableAlpha
+                        />
                     </PanelRow>
                 </PanelBody>
 
+                <PanelBody title="Block Title">
+                    <TextControl
+                        label="Title"
+                        value={attributes.blockTitle}
+                        onChange={(newTitle) => setAttributes({ blockTitle: newTitle })}
+                    />
+                </PanelBody>
+
+                {attributes.cards.map((card, index) => (
+                    <PanelBody key={index} title={`Card ${index + 1}`} initialOpen={false}>
+                        <Button variant="primary" onClick={() => removeCard(index)}>
+                            Remove Card
+                        </Button>
+                        <SelectControl
+                            label="Icon"
+                            value={card.icon}
+                            options={[
+                                { label: "Smiley", value: "smiley" },
+                                { label: "Heart", value: "heart" },
+                                { label: "Lightbulb", value: "lightbulb" },
+                            ]}
+                            onChange={(newIcon) => handleCardChange({ ...card, icon: newIcon }, index)}
+                        />
+                        <TextControl
+                            label="Card Title"
+                            value={card.title}
+                            onChange={(newTitle) => handleCardChange({ ...card, title: newTitle }, index)}
+                        />
+                        <TextControl
+                            label="Card Description"
+                            value={card.description}
+                            onChange={(newDescription) => handleCardChange({ ...card, description: newDescription }, index)}
+                        />
+                        <TextControl
+                            label="Current Price"
+                            value={card.currentPrice}
+                            onChange={(newPrice) => handleCardChange({ ...card, currentPrice: newPrice }, index)}
+                        />
+                        <TextControl
+                            label="Currency"
+                            value={card.currency}
+                            onChange={(newCurrency) => handleCardChange({ ...card, currency: newCurrency }, index)}
+                        />
+                        <ToggleControl
+                            label="Has Discount"
+                            checked={card.hasDiscount}
+                            onChange={(newHasDiscount) => handleCardChange({ ...card, hasDiscount: newHasDiscount }, index)}
+                        />
+                        {card.hasDiscount && (
+                            <TextControl
+                                label="Discount Price"
+                                value={card.discountPrice}
+                                onChange={(newDiscountPrice) => handleCardChange({ ...card, discountPrice: newDiscountPrice }, index)}
+                            />
+                        )}
+                        <TextControl
+                            label="Button Text"
+                            value={card.button}
+                            onChange={(newButton) => handleCardChange({ ...card, button: newButton }, index)}
+                        />
+                    </PanelBody>
+                ))}
+
+                <Button variant="primary" onClick={addNewCard}>
+                    Add New Card
+                </Button>
             </InspectorControls>
 
-            <div className="icon-container">
-                <span className={`dashicons dashicons-${props.attributes.icon}`} />
+            <div className="card-preview">
+                <RichText
+                    tagName="h2"
+                    className="block-title"
+                    value={attributes.blockTitle}
+                    onChange={(newTitle) => setAttributes({ blockTitle: newTitle })}
+                    placeholder="Block Title"
+                />
+                <div className="cards-container">
+                    {attributes.cards.map((card, index) => (
+                        <div key={index} className="card">
+                            <span className={`dashicons dashicons-${card.icon}`}></span>
+                            <h3>{card.title}</h3>
+                            <p>{card.description}</p>
+                            <div className="pricing-table-price">
+                                {card.hasDiscount && card.discountPrice ? (
+                                    <>
+                                        <span className="previous-price">{card.currency}{card.currentPrice}</span>
+                                        <span className="current-price">{card.currency}{card.discountPrice}</span>
+                                    </>
+                                ) : (
+                                    <span className="current-price">{card.currency}{card.currentPrice}</span>
+                                )}
+                            </div>
+                            <button className="btn">{card.button}</button>
+                        </div>
+                    ))}
+                </div>
             </div>
-            {/* <h2>Hello from Edit block</h2> */}
-            <RichText allowedFormats={["core/bold", "core/italic"]} tagName="h3" className={`headline`} onChange={handleTitleChange} value={props.attributes.title} placeholder="Card Title"/>
-
-            <RichText allowedFormats={[]} tagName="p" className={`description`} onChange={handleDescriptionChange} value={props.attributes.description} placeholder="Card Description"/>
-
-
-            <div className="pricing-table-price">
-                {props.attributes.hasDiscount && props.attributes.discountPrice ? (
-                    <>
-                        <span className="previous-price">{props.attributes.currency}{props.attributes.currentPrice}</span>
-                        <span className="current-price">{props.attributes.currency}{props.attributes.discountPrice}</span>
-                    </>
-                ) : (
-                    <span className="current-price">{props.attributes.currency}{props.attributes.currentPrice}</span>
-                )}
-            </div>
-
-            <Button variant="primary">
-                <RichText allowedFormats={["core/bold", "core/italic"]} className={`btn`} value={props.attributes.button} onChange={handleButtonChange} placeholder="BUY"/>
-            </Button>
-            
         </div>
-    )
+    );
 }
